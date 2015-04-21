f0be7000ecc3f32a91648bb1f249336d4fb1720c
Dmitry Kireev <dmitry@kireev.co>
2015-04-20 17:51:21 -0700

    Add gd library

--- openresty/rpm/build_openresty-rpm.ubuntu.sh
+++ openresty/rpm/build_openresty-rpm.ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 apt-get -y install make ruby1.9.1 ruby1.9.1-dev \
-git-core libpcre3-dev libxslt1-dev libgd2-xpm-dev \
+git-core libpcre3-dev libxslt1-dev libgd2-xpm-dev libgd2-xpm \
 libgeoip-dev unzip zip build-essential gem
+
 
 gem install fpm
 
