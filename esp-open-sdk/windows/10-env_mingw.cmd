7205f3383787cff91df9d54f07861b91f6bd0ddc
Dmitry Kireev <dmitry@kireev.co>
2015-04-25 09:24:50 -0700

    cleanup

--- esp-open-sdk/windows/10-env_mingw.cmd
+++ esp-open-sdk/windows/10-env_mingw.cmd
@@ -4,41 +4,34 @@ set ESPRESSIF_HOME=c:/Espressif
 echo Installing Chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-echo Enabling case sensitivity
-REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "obcaseinsensitive" /t REG_DWORD /d 0 /f
+echo Add repository
+choco sources add -name kireevco -source 'https://www.myget.org/F/kireevco-chocolatey/'
 
-echo Installing MingGW
-choco install mingw -y
+echo Installing wget
+choco install wget -y
+
+echo Installing Git
+choco install git -y
+
+echo Installing MingGW-get (pulls down mingw too)
 choco install mingw-get -y
 
+echo Adding git to the PATH
+setx /M PATH "%programfiles(x86)%\Git\bin;%PATH%"
+echo Adding ENV variables
 setx /M PATH "c:\tools\mingw64\bin\;%PATH%"
 setx /M PATH "c:\tools\mingw64\include\;%PATH%"
 setx /M PATH "c:\tools\mingw64\msys\1.0\bin\;%PATH%"
 setx /M PATH "c:\tools\mingw64\msys\1.0\include\;%PATH%"
 
+::echo Enabling case sensitivity (???not required with mingw & msys)
+::REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\kernel" /v "obcaseinsensitive" /t REG_DWORD /d 0 /f
 
-echo Installing Git
-choco install git -y
+echo Installing required mingw components
 
-
-echo Adding git to the path
-setx /M PATH "%programfiles(x86)%\Git\bin;%PATH%"
-
-::echo Installing cyg-get chocolatey provider
-::choco install cyg-get -source 'https://www.myget.org/F/kireevco-chocolatey/'
-
-echo Installing required components
-::choco install ^
-:: git mingw32-base mingw32-mgwport mingw32-pdcurses mingw32-make mingw32-autoconf mingw32-automake mingw32-gdb ^
-:: gdb automake make autoconf pdcurses gperf bison flex texinfo gawk ncurses libncurses-devel libexpat-dev wget libtool patch mktemp m4 mgwport gcc-core gcc-tools gcc-c++ libz bzip2 libintl-devel ^
-:: msys-base msys-coreutils msys-gcc-bin msys-wget-bin msys-m4 msys-bison-bin msys-flex-bin msys-gawk msys-sed msys-autoconf msys-automake msys-mktemp msys-patch msys-libtool ^
-:: -source cygwin
-
-::z
-::choco install git gperf bison flex patch make automake libtool subversion gcc-core gcc-g++ catgets wget findutils libncursesw-devel libncurses-devel gettext libexpat-devel -source cygwin
 mingw-get install mingw32-base mingw32-mgwport mingw32-pdcurses mingw32-make mingw32-autoconf mingw32-automake mingw-developer-toolkit ^
  mingw-developer-toolkit  mingw32-gdb gcc gcc-c++ libz bzip2 msys-base msys-coreutils msys-coreutils-ext msys-gcc-bin msys-wget-bin ^
- msys-m4 msys-bison-bin msys-flex-bin msys-gawk msys-regex msys-libregex msys-sed msys-autoconf msys-gperf msys-automake msys-mktemp msys-patch msys-libtool 
+ msys-m4 msys-bison-bin msys-flex-bin msys-gawk msys-make msys-regex msys-libregex msys-sed msys-autoconf msys-gperf msys-automake msys-mktemp msys-patch msys-libtool 
 
 ::choco install mingw-gcc -source cygwin
 ::??choco install mingw64-x86_64-gcc-g++ -source cygwin
