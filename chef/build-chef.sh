6c83bb55d205c878ad77e305fcabd2191be2663d
Dmitry Kireev <dmitry@kireev.co>
2015-07-23 11:44:20 -0700

    add mips-mipsel workaround

--- chef/build-chef.sh
+++ chef/build-chef.sh
@@ -10,15 +10,27 @@ FILENAME="${PROJECT_NAME}_${VERSION}-${BUILD_NUMBER}_${ARCH}.deb"
 # export ARCH="mipsel"
 
 rm -rf omnibus-chef
-rm -rf ${PROJECT_NAME}_*-${BUILD_NUMBER}_${ARCH}.deb
+rm -rf ${PROJECT_NAME}_*.deb
 git clone https://github.com/chef/omnibus-chef
 cd omnibus-chef
 git checkout $VERSION
 
 bundle install --without development
-
-
 bundle exec omnibus build chef
 
+echo "Patching for ${ARCH}"
+cd $BUILD_DIR/omnibus-chef/pkg
+mkdir -p ${PROJECT_NAME}_repack
+dpkg-deb -x ${PROJECT_NAME}_*.deb ${PROJECT_NAME}_repack
+dpkg-deb -e ${PROJECT_NAME}_*.deb ${PROJECT_NAME}_repack/DEBIAN
+
+# if [[ $(uname -n) == "debian-${ARCH}" ]]
+# then
+  sed -i "s/Architecture: mips\b/Architecture: mipsel/g" ${BUILD_DIR}/omnibus-chef/pkg/${PROJECT_NAME}_repack/DEBIAN/control  
+# fi
+
+
+dpkg-deb -b ${PROJECT_NAME}_repack ${PROJECT_NAME}_${VERSION}-${BUILD_NUMBER}_${ARCH}.deb
+
 # echo "Pushing to bintray"
-curl -T $(ls ${PROJECT_NAME}_*-${BUILD_NUMBER}_${ARCH}.deb) -ukireevco:$1 https://api.bintray.com/content/kireevco/deb/$PROJECT_NAME/$VERSION/$FILENAME
+curl -T ${PROJECT_NAME}_${VERSION}-${BUILD_NUMBER}_${ARCH}.deb -ukireevco:$1 https://api.bintray.com/content/kireevco/deb/$PROJECT_NAME/$VERSION/$FILENAME
