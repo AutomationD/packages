0ded6b028f94e1751cde5077567280498a7c4685
Dmitry Kireev <dmitry@kireev.co>
2015-10-25 11:35:39 +0300

    update ncurses patches

--- esp-open-sdk/windows/cygwin/20-build_cygwin.sh
+++ esp-open-sdk/windows/cygwin/20-build_cygwin.sh
@@ -64,12 +64,12 @@ wget http://invisible-island.net/datafiles/release/ncurses.tar.gz
 tar -zxf ncurses.tar.gz
 
 cd ncurses-*
-sed -i 's/#include <curses.priv.h>/#include <curses.priv.h>\\n#include <sys\/time.h>/g' ./ncurses/win32con/gettimeofday.c
+sed -i 's/#include <curses.priv.h>/#include <curses.priv.h>\n#include <sys\/time.h>/g' ./ncurses/win32con/gettimeofday.c
 sed -i 's/void \*tz GCC_UNUSED/struct timezone \*tz/g' ./ncurses/win32con/gettimeofday.c
-sed -i 's/#include <curses.priv.h>/#include <curses.priv.h>\\n#include <windows.h>\\n#define ATTACH_PARENT_PROCESS (DWORD)-1/g' ./ncurses/win32con/win_driver.c
+sed -i 's/#include <curses.priv.h>/#include <curses.priv.h>\n#include <windows.h>\\n#define ATTACH_PARENT_PROCESS (DWORD)-1/g' ./ncurses/win32con/win_driver.c
 
 sed -i 's/double fraction = 0.0;//g' ./test/tclock.c
-sed -i 's/setlocale/double fraction = 0.0;\\nsetlocale/g' ./test/tclock.c
+sed -i 's/setlocale/double fraction = 0.0;\nsetlocale/g' ./test/tclock.c
 
 
 ./configure --enable-term-driver --enable-sp-funcs --prefix=/usr --without-ada
